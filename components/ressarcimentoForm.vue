59c4413794377e0fd97c3eabec39f0e38abbaab0
boloko <yurifbeck@gmail.com>
2021-09-25 20:23:48 -0300

    pdf reports wow

--- components/ressarcimentoForm.vue
+++ components/ressarcimentoForm.vue
@@ -23,17 +23,19 @@
           :items="contas"
           label="Conta recebedora"
           item-text="nome"
+          return-object
           dense
           outlined
         ></v-select>
-        <!-- <v-select
+        <v-select
           v-model="devedor"
           :items="contas"
           label="Conta devedora"
           item-text="nome"
+          return-object
           dense
           outlined
-        ></v-select> -->
+        ></v-select>
       </v-col>
       <v-col cols="12">
         <v-data-table
@@ -72,10 +74,16 @@
         </v-data-table>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-btn @click="generatePDF">Gerar PDF</v-btn>
+      </v-col>
+    </v-row>
   </v-form>
 </template>
 
 <script>
+// import jsPDFInvoiceTemplate from 'jspdf-invoice-template'
 export default {
   props: {
     isEdit: {
@@ -126,6 +134,15 @@ export default {
       obra: {},
     }
   },
+  head() {
+    return {
+      script: [
+        {
+          src: 'https://unpkg.com/jspdf-invoice-template@latest/dist/index.js',
+        },
+      ],
+    }
+  },
 
   computed: {
     items() {
@@ -134,7 +151,7 @@ export default {
           e.obraId === this.obra &&
           e.date > new Date(this.dates[0]) &&
           e.date < new Date(this.dates[1]) &&
-          e.pagador === this.recebedor
+          e.pagador === this.recebedor.nome
       )
     },
     pagamentos() {
@@ -144,6 +161,8 @@ export default {
           const item = {}
           item.obra = compra.obra.nome
           item.obraId = compra.obra.id
+          item.descricao = compra.descricao
+          item.fornecedor = compra.fornecedor.nome
           item.valor = pagamento.valor
           item.key = pagamento.valor + '_' + pagamento.date
           item.date = new Date(pagamento.date)
@@ -155,8 +174,92 @@ export default {
       })
       return pagamentos
     },
+    total() {
+      return 130
+    },
   },
 
-  methods: {},
+  methods: {
+    generatePDF() {
+      const pdfConfig = {
+        outputType: 'save',
+        returnJsPDFDocObject: true,
+        fileName: 'Ressarcimento ' + this.recebedor.nome,
+        orientationLandscape: false,
+        logo: {
+          src: 'https://raw.githubusercontent.com/bolokoz/engebeck/main/assets/logo_transparent.png',
+          width: 53.33, // aspect ratio = width/height
+          height: 26.66,
+          margin: {
+            top: 0, // negative or positive num, from the current position
+            left: 0, // negative or positive num, from the current position
+          },
+        },
+        business: {
+          name: 'EngeBeck',
+          address: 'Rua Conselheiro Furtado, 123',
+          phone: '(67) 999767835 ',
+          email: 'yuri@engebeck.com.br',
+          website: 'www.engebeck.com.br',
+        },
+        contact: {
+          label: 'Relatório destinado à:',
+          name: 'MRB Investimentos LTDA',
+          address: 'Av. Afonso Pena, 2131, sala 2203',
+          phone: '(67) 981238383',
+          email: 'mrbinvestagroind@gmail.com',
+        },
+        invoice: {
+          label: 'Relatório de ressarcimento ' + this.obra.nome,
+          num: 19,
+          invDate: this.dates[0],
+          invGenDate: 'Invoice Date: 02/02/2021 10:17',
+          headerBorder: false,
+          tableBodyBorder: false,
+          header: ['#', 'Descrição', 'Fornecedor', 'Data', 'Valor'],
+          table: this.selected.map((d, i) => [
+            i + 1,
+            d.descricao,
+            d.fornecedor,
+            d.date,
+            d.valor,
+          ]),
+          invTotalLabel: 'SubTotal:',
+          invTotal: '100.000,00',
+          invCurrency: 'R$',
+          row1: {
+            col1: 'Administração:',
+            col2: '10',
+            col3: '%',
+            style: {
+              fontSize: 10, // optional, default 12
+            },
+          },
+          row2: {
+            col1: 'Total:',
+            col2: '116,199.90',
+            col3: 'R$',
+            style: {
+              fontSize: 14, // optional, default 12
+            },
+          },
+          invDescLabel: '',
+          invDesc:
+            "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.",
+        },
+        footer: {
+          text: '--',
+        },
+        pageEnable: true,
+        pageLabel: 'Relatório',
+      }
+
+      jsPDFInvoiceTemplate.default(pdfConfig)
+      //   const doc = new jsPDF()
+
+      //   doc.text('Hello world!', 10, 10)
+      //   doc.save('a4.pdf')
+    },
+  },
 }
 </script>
